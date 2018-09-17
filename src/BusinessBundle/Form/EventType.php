46c06975eae87f7778ead266ff86aebb672e7508
gueyebaba <gueybaba@gmail.com>
2018-09-17 10:20:55 +0200

    api implementation

---
+++ src/BusinessBundle/Form/EventType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace BusinessBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EventType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('customerRef')->add('name')->add('date')->add('detailedDescription')->add('webSite')->add('country')->add('location')->add('city')->add('typeOfEvent')->add('industry')->add('thematicTag')->add('nameOfOrganizer')->add('contactForm')->add('attachment')->add('socialMediaSharing')->add('status')->add('visuel');
+    }/**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'BusinessBundle\Entity\Event'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'businessbundle_event';
+    }
+
+
+}
---
+++ src/BusinessBundle/Form/MediaType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace BusinessBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class MediaType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('type')->add('uri')->add('event');
+    }/**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'BusinessBundle\Entity\Media'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'businessbundle_media';
+    }
+
+
+}
